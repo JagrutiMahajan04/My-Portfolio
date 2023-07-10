48ac29d4f5352b643d6a867a17351ae136cfcc67
JagrutiMahajan04 <jagrutimaha2000@gmail.com>
2023-07-10 15:52:39 +0530

    Scroll Reveal & typed js

--- script.js
+++ script.js
@@ -36,6 +36,28 @@ window.onscroll = () => {
 /*======================================= remove toggle icon and navbar when click navbar link (scroll)   ==========================================================*/
 menuIcon.classList.remove('bx-x');
 navbar.classList.remove('active');
-
 };
+
+/*========================================== scroll reveal   ==========================================================*/
+ScrollReveal({
+    // reset: true,
+     distance: '80px',
+     duration: 2000,
+     delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .skills-container, .project-box, .contact form',  { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/*========================================== typed js  ==========================================================*/
+const typed = new Typed('.multiple-text',{
+    strings:['Web developer','Frontend Developer','Backend learner'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
+
 
